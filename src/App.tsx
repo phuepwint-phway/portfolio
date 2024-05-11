bd9e344c45b99f6704ecaa00aa98062ac0c0ebf9
phuepwint-phway <phuepwintphway@gmail.com>
2024-05-11 09:21:52 +0630

    useRef hook

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ import { ThemeContextProvider } from './components/context/ThemeContext';
 import { Box } from './components/context/Box';
 import { User } from './components/context/User';
 import { UserContextProvider} from './components/context/UserContext';
+import { DomRef } from './components/ref/DomRef';
+import { MutableRef } from './components/ref/MutableRef';
 
 function App() {
 
@@ -69,6 +71,8 @@ function App() {
             <UserContextProvider>
                 <User />
             </UserContextProvider>
+            <DomRef />
+            <MutableRef />
         </div>
     )
 }
